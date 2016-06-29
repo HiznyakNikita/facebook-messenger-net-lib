7ff06d30893cba368b2b833e4a74704efcf545af
HiznyakNikita <hiznyaknikita1996@gmail.com>
2016-06-29 18:57:42 +0400

    add WelcomeMessage data class

---
+++ FacebookMessengerLib/FacebookMessengerLib/API.Types/WelcomeMessage.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookMessengerLib.API.Types
+{
+    public class WelcomeMessage
+    {
+        [JsonProperty(PropertyName = "setting_type", Required = Required.Always)]
+        public string SettingType { get; private set; }
+        [JsonProperty(PropertyName = "thread_state", Required = Required.Always)]
+        public string ThreadState { get; private set; }
+        [JsonProperty(PropertyName = "call_to_actions", Required = Required.Always)]
+        public IList<Message> CallToActions { get; set; }
+
+        public WelcomeMessage()
+        {
+            SettingType = "call_to_actions";
+            ThreadState = "new_thread";
+        }
+        public WelcomeMessage(IList<Message> callToActions)
+        {
+            SettingType = "call_to_actions";
+            ThreadState = "new_thread";
+            CallToActions = callToActions;
+        }
+    }
+}
